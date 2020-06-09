866ac91b5b6013c812c890d80907e6431fb4fbc3
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-09 23:38:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,6 +10,12 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+
+  this_hash = base_hash
+  puts this_hash
+
+  this_hash[:railroads] = [{pieces: 4}]
+
   new_hash = {
     railroads: {
       pieces: 4,
